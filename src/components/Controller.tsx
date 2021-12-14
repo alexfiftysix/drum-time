ad3b8612b2b12ea38c8b8cc66c443876ad072a19
Alex Miller <alex.miller@readify.net>
2021-12-14 13:37:27 +1000

    Add a kick

--- src/components/Controller.tsx
+++ src/components/Controller.tsx
@@ -1,8 +1,10 @@
 import { Looper } from "./Looper";
 import { Bleep } from "./Bleep";
+import { Kick } from "./Kick";
 
 export const Controller = () => (
   <div>
+    <Kick />
     <Bleep />
     <Looper />
   </div>
