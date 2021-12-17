fb85b6eef2d1851d2c5df37443e1551e236b71ee
Alex Miller <alex.miller@readify.net>
2021-12-17 15:13:32 +1000

    Add a slider to tempo setter

--- src/components/sequencer/tempoSetter/TempoSetter.tsx
+++ src/components/sequencer/tempoSetter/TempoSetter.tsx
@@ -1,6 +1,9 @@
 import styles from './TempoSetter.module.scss'
-import { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import * as Tone from 'tone'
+
+const minTempo = 1
+const maxTempo = 300
 
 export const TempoSetter = () => {
   const [tempo, setTempo] = useState(120)
@@ -9,14 +12,33 @@ export const TempoSetter = () => {
     Tone.Transport.bpm.rampTo(tempo, 0.1)
   }, [tempo])
 
+  const handleSetTempo = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      const value = parseInt(event.target.value)
+      if (Number.isNaN(value) || value < minTempo || value > maxTempo) return
+      setTempo(value)
+    },
+    [setTempo]
+  )
+
   return (
     <label className={styles.root}>
       <span className={styles.label}>bpm</span>
       <input
+        type="range"
+        min={minTempo}
+        max={maxTempo}
+        step={1}
+        onInput={handleSetTempo}
+        value={tempo}
+      />
+      <input
+        className={styles.numberInput}
         type="number"
+        min={minTempo}
+        max={maxTempo}
         step={0}
-        // @ts-ignore
-        onInput={(event) => setTempo(event.target.value)}
+        onInput={handleSetTempo}
         value={tempo}
       />
     </label>
